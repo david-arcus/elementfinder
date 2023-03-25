e8e1fe15e4f6d72534f5f1f25d89c6b4732e7735
David Arcus <arcus.david@gmail.com>
2023-03-25 18:28:47 +1100

    collisions mostly figured out

--- src/js/main.js
+++ src/js/main.js
@@ -5,8 +5,8 @@ class Main {
         this.userInput = document.querySelector('.user-input');
         this.submit = document.querySelector('.submit');
         this.displayUserName = document.querySelector('.display-user-name');
-        this.foundElements = new Map();
-        this.selectedElements = new Map();
+        this.foundElements;
+        this.selectedElements;
     }
 
     init() {
@@ -19,20 +19,39 @@ class Main {
 
     findElements() {
 
+        this.foundElements = new Map();
+        this.selectedElements = new Map();
+
         elements.forEach(element => {
 
+            // do a global, case-insensitive search in user's name for element
             const regExp = new RegExp(element.symbol, "gi");
+            // spread operator executes the regex
             const matches = [...this.userInput.value.matchAll(regExp)];    
 
             if (matches.length > 0) {
 
+                // gets the starting index for every element
                 let foundIndexes = matches.map(match => {
                     return match.index;
                 });
 
+                // enumerate all indexes used by an element
+                // we'll call them slots
+                let slots = [];
+                foundIndexes.forEach(index => {
+                    slots.push(index);
+                    if (element.symbol.length > 1) {
+                        slots.push(index+element.symbol.length-1);
+                    }
+                });
+
+                // gives us every element, starting index and positions
+                // of every found element
                 this.foundElements.set(element.symbol, {
                     foundIndexes,
-                    length:element.symbol.length
+                    length:element.symbol.length,
+                    slots:slots
                 });
 
             }
@@ -44,6 +63,15 @@ class Main {
     }
 
     generateElementButtons() {
+
+        // generate a button for every found element in user's name
+
+        if (this.foundElements.size == 0) {
+            console.log('there are no elements in your name!')
+            return;
+        }
+
+        this.displayUserName.innerHTML = this.userInput.value;
 
         const buttonElements = [];
         let buttonsContainer = document.querySelector('.buttons-container');
@@ -67,43 +95,87 @@ class Main {
 
     renderElementsInName(event) {
 
+        // periodic table elements are unique, so we can use 
+        // them as data IDs to find which one was clicked
         let clickedElement = event.target.dataset.element;
-        let nameArray = this.userInput.value.split('');
+        let userNameArray = this.userInput.value.split('');
+        let requestedSlots;
 
-        // is this element already selected?
         let elementDetails = this.foundElements.get(clickedElement);
 
+        // check if element is already selected
         if (this.selectedElements.get(clickedElement) == undefined) {
+            // toggle on
             this.selectedElements.set(clickedElement, elementDetails);
+            // get all the indexes of where this element occurs 
+            requestedSlots = this.selectedElements.get(clickedElement).slots
             event.target.classList.add('selected');
         } else {
+            // toggle off
+            requestedSlots = '';
             event.target.classList.remove('selected');
+            // remove selected element from the map
             this.selectedElements.delete(clickedElement);
         }
 
+        // loop through every selected element
         for (let [selectedElement] of this.selectedElements) {  
 
-            // check for collisions
+            // element is being toggled on
+            if (requestedSlots) {
 
-            
+                // check for collisions (if at least one element has already been selected)
+                if (this.selectedElements.size > 1) {
 
-            this.selectedElements.get(selectedElement).foundIndexes.forEach(index => {
-                if (selectedElement.length === 1) {
-                    nameArray[index] = `<strong>${nameArray[index]}</strong>`;
-                } else {
-                    nameArray[index] = `<strong>${nameArray[index]}`;
+                    // exclude the element being added
+                    if (selectedElement != clickedElement) {
 
-                    nameArray[index+selectedElement.length-1] =
-                    `${nameArray[index+selectedElement.length-1]}</strong>`;
+                        const collision = this.selectedElements.get(selectedElement).slots.filter(element =>  {
+                            return requestedSlots.includes(element)
+                        });
+
+                        if (collision.length > 0) {
+                            console.log(`collision! at: ${collision}`);
+                            console.log(`collision with: ${selectedElement}`);
+                            console.log(`clicked element: ${clickedElement}`);
+
+                            if (this.selectedElements.get(selectedElement).foundIndexes.length == 1) { 
+                                // we can remove the element if there's only one instance of it
+                                this.selectedElements.delete(selectedElement);
+                                document.querySelector(`[data-element=${selectedElement}]`).classList.remove('selected');
+                            } else if (this.selectedElements.get(selectedElement).foundIndexes.length > 1) {
+                                // retain other instances of element that there isn't a collision with
+                            }
+
+                        }
+
+                    }
                 }
-            })
+
+            }
+
+            // check if element has been deleted due to collision
+            if (this.selectedElements.get(selectedElement) != undefined) {
+
+                this.selectedElements.get(selectedElement).foundIndexes.forEach(index => {
+                    if (selectedElement.length === 1) {
+                        userNameArray[index] = `<strong>${userNameArray[index]}</strong>`;
+                    } else {
+                        userNameArray[index] = `<strong>${userNameArray[index]}`;
+
+                        userNameArray[index+selectedElement.length-1] =
+                        `${userNameArray[index+selectedElement.length-1]}</strong>`;
+                    }
+                })
+
+            }
 
         }
 
-        console.log(this.selectedElements);      
+        // console.log(this.selectedElements);      
 
-        this.displayUserName.innerHTML = nameArray.join('');
-        // document.getElementById('foo').innerText = this.foundElements;
+        this.displayUserName.innerHTML = userNameArray.join('');
+        document.getElementById('foo').innerText = JSON.stringify(Array.from(this.selectedElements.entries()), null, 2);
     }
 }
 
